87a601d99a9310ab386a8553ccff142aa32f167d
devarktini <arktiniinnovations.com>
2025-11-01 20:10:57 +0530

    code added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaFacebookF, FaInstagram, FaXTwitter, FaLinkedinIn } from 'react-icons/fa6';
-import logo from '@/public/images/logo.png';
+import logo from '@/public/images/Logo.png';
 import rLogo from '@/public/images/rMarkW.png';
 
 export default function Footer() {
