7512eefdbbcd20a8909b6ab0035d28d65efd2ecb
abhishekyadav4 <yadavabhishek487@gmail.com>
2026-02-11 22:06:19 +0530

    added robots.txt file

---
+++ app/robots.ts
@@ -0,0 +1,71 @@
+// app/robots.ts
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: [
+                    '/login/',
+                    'https://admin.teachercool.com/',
+                    '/dashboard/',
+                    '/admin/',
+                   ' /api/',
+                    '/cgi-bin/',
+                ],
+            },
+
+            // AI bots (optional)
+            {
+                userAgent: 'GPTBot',
+                allow: '/',
+            },
+            {
+                userAgent: 'Google-Extended',
+                allow: '/',
+            },
+            {
+                userAgent: 'PerplexityBot',
+                allow: '/',
+            },
+            {
+                userAgent: 'ClaudeBot',
+                allow: '/',
+            },
+        ],
+        sitemap: 'https://www.teacher.com/sitemap.xml',
+    };
+}
+
+
+
+
+// import type { MetadataRoute } from 'next'
+
+// export default function robots(): MetadataRoute.Robots {
+//   return {
+//     rules: [
+//       {
+//         userAgent: 'GPTBot',
+//         allow: '/',
+//       },
+//       {
+//         userAgent: 'Google-Extended',
+//         allow: '/',
+//       },
+//       {
+//         userAgent: 'PerplexityBot',
+//         allow: '/',
+//       },
+//       {
+//         userAgent: 'ClaudeBot',
+//         allow: '/',
+//       },
+//     ],
+//     sitemap: 'https://www.equipmedy.com/sitemap.xml',
+//   };
+// }
+
+
